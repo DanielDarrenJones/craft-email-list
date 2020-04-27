2e8569aaeae48d2f5c1fb9d3dc3c01f369dcd313
Luke Hopkins <arrowtheknee@gmail.com>
2020-04-27 19:15:44 +0100

    Initial commit

---
+++ src/services/Email.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Email List plugin for Craft CMS 3.x
+ *
+ * A plugin for saving emails to a list
+ *
+ * @link      https://github.com/luke-hopkins
+ * @copyright Copyright (c) 2020 Luke Hopkins
+ */
+
+namespace lukehopkins\emaillist\services;
+
+use lukehopkins\emaillist\EmailList;
+
+use Craft;
+use craft\base\Component;
+
+/**
+ * Email Service
+ *
+ * All of your plugin’s business logic should go in services, including saving data,
+ * retrieving data, etc. They provide APIs that your controllers, template variables,
+ * and other plugins can interact with.
+ *
+ * https://craftcms.com/docs/plugins/services
+ *
+ * @author    Luke Hopkins
+ * @package   EmailList
+ * @since     1.0.0
+ */
+class Email extends Component
+{
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * This function can literally be anything you want, and you can have as many service
+     * functions as you want
+     *
+     * From any other plugin file, call it like this:
+     *
+     *     EmailList::$plugin->email->exampleService()
+     *
+     * @return mixed
+     */
+    public function exampleService()
+    {
+        $result = 'something';
+
+        return $result;
+    }
+}
